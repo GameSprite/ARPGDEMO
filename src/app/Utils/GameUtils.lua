d33146d8e7ab4b1ffca1625b58d759678e779685
GameSprite <student@chenguanzhou.local>
2016-05-17 16:43:56 +0800

    change  square operation
    change  square operation

--- src/app/Utils/GameUtils.lua
+++ src/app/Utils/GameUtils.lua
@@ -58,7 +58,8 @@ end
      @
 --]]
 function distance(p1, p2)
-  return math.sqrt((p1.x-p2.x)^2+(p1.y-p2.y)^2)
+  --return math.sqrt((p1.x-p2.x)^2+(p1.y-p2.y)^2)
+  return math.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y))
 end
 --[[
     求节点对应的模型
